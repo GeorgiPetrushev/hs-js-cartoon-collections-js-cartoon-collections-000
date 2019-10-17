966f4fa6b273357fe9ad72560d7cdb5ed15baa1c
Georgi Petrushev <figoto660@gmail.com>
2019-10-17 18:38:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
-function dwarfRollCall(dwarves) {
+let newDwarves = [];
+
+
+let dwarfRollCall =(dwarves) => {
+
+for(let i =0;i<dwarves.length ; i++ ){
+
+  newDwarves.push(`${i+1}. ${dwarves[i]} `);
+}
+return newDwarves.join("");
 }
 
-function summonCaptainPlanet(planeteerCalls){
+// function dwarfRollCall(dwarves) {
+//   let j = 1;
+//   return dwarves.map(function(x){
+//     return `${j++}. ${x}`;
+//   })
+  
+// }
+
+
+let toUpperCaseArr = [];
+let summonCaptainPlanet = (planeteerCalls)=>{
+
+toUpperCaseArr =  planeteerCalls.map(function(x){
+  return `${x.toUpperCase()}!`;
+})
+return toUpperCaseArr;
 }
+
 
 function longPlaneteerCalls(words) {
+
+
+    for(let i=0; i<words.length;i++)
+    {
+      if(words[i].length >4){return true;}
+    }
+    return false;
+  
 }
 
-function findTheCheese (foods) {
+// let findTheCheese =(foods) => {
+// for(let i=0; i<foods.length;i++){
+//     if(foods[i]==="gouda"){return "gouda";}
+      
+//     }
+//   for(let i=0; i<foods.length;i++){
+//     if(foods[i]==="cheddar"){return "cheddar";}
+//   }  
+//   return "no cheese!";
+// }
+
+
+let findTheCheese = (foods) =>{
+  let newFoods = [];
+    for(let i = 0; i < foods.length; i++){
+      if(foods[i] === "cheddar" || foods[i] ==="gouda"){          newFoods.push(`${foods[i]}`);
+        }
+      }
+
+      if(newFoods.length >0){
+        return  `${newFoods}`
+        };
+        return "no cheese!";
 }
+
